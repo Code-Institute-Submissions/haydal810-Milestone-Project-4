8e22bb6729ad5e604cc1a11c4fe99e55b3afe529
David <haydal.tmps@gmail.com>
2019-11-28 23:53:58 +0000

    modify def login in views.py in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -2,19 +2,33 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from accounts.forms import UserLoginForm
 
-# Create your views here.
-
 def index(request):
     """Return the index.html file"""
-    return render(request, 'index.html')
+    return render(request,  'index.html')
+
 
 def logout(request):
     """Log the user out"""
     auth.logout(request)
-    messages.success(request, "You have successfully logged out")
+    messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
-    
+
+
 def login(request):
-    """Return the login page"""
-    login_form = UserLoginForm()
-    return render(request, 'login.html', {"login_form": login_form})
+    """Return a login page"""
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+            
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
