762482f8d10a918facc8f3f8fa731601c14a9e69
David <haydal.tmps@gmail.com>
2019-12-02 22:05:32 +0000

    fix bug on line 47 on views.py

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,6 @@ def index(request):
     """Return the index.html file"""
     return render(request,  'index.html')
 
-
 @login_required
 def logout(request):
     """Log the user out"""
@@ -21,21 +20,19 @@ def login(request):
     """Return a login page"""
     if request.user.is_authenticated:
         return redirect(reverse('index'))
-
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
-                                     password=request.POST['password'])
+                                    password=request.POST['password'])
+            messages.success(request, "You have successfully logged in!")
 
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!")
                 return redirect(reverse('index'))
             else:
-                login_form.add_error(
-                    None, "Your username or password is incorrect")
+                login_form.add_error(None, "Your username or password is incorrect")
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
@@ -47,22 +44,20 @@ def registration(request):
         return redirect(reverse('index'))
 
     if request.method == "POST":
-        registration_form = UserLoginForm(request.POST)
+        registration_form = UserRegistrationForm(request.POST)
 
         if registration_form.is_valid():
             registration_form.save()
 
-        user = auth.authenticate(username=request.POST['username'],
-                                password=request.POST['password1'])
-                                
-        if user:
-            auth.login(user=user, request=request)
-            messages.success(request, "You have successfully registered with us")
-            return redirect(reverse('index'))
-        else:
-            messages.error(request, "Unable to register your account, with us, at this time")
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered")
+            else:
+                messages.error(request, "Unable to register your account at this time")
+    else:
+        registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
 
-
-    registration_form = UserRegistrationForm
-    return render(request, 'registration.html',
-                  {"registration_form": registration_form})
