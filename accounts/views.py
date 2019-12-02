4f2e88433dbb63fe4d16af964408e1b9abf1bf7f
David <haydal.tmps@gmail.com>
2019-12-02 20:23:54 +0000

    update link for register nav item and html page

--- accounts/views.py
+++ accounts/views.py
@@ -37,4 +37,8 @@ def login(request):
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+def registration(request):
+    """Render the registration page"""
+    return render(request, 'registration.html')
     
