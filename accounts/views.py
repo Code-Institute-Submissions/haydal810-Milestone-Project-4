ae0a7d79af2b5e268dc55a804122fec02e9180f5
David <haydal.tmps@gmail.com>
2019-12-02 19:15:14 +0000

    add return redirect to views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 def index(request):
     """Return the index.html file"""
     return render(request,  'index.html')
 
-
+@login_required
 def logout(request):
     """Log the user out"""
     auth.logout(request)
@@ -16,6 +17,9 @@ def logout(request):
 
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -27,6 +31,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
