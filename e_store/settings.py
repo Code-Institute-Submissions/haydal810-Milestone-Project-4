32fe14bb39eccc823906068c31f8a9444ad1ae81
David <haydal.tmps@gmail.com>
2019-12-03 16:33:30 +0000

    add custom authentication in backends.py

--- e_store/settings.py
+++ e_store/settings.py
@@ -124,9 +124,14 @@ MESSAGE_STORAGE  ="django.contrib.messages.storage.session.SessionStorage"
 
 #EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+#This allows email to be sent from gmail to the user to reset password:
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
