597133d9791863c9dc161eb55094012de2d12bc2
David <haydal.tmps@gmail.com>
2019-12-14 14:53:43 +0000

    add products.html, urls and model

--- e_store/settings.py
+++ e_store/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -139,6 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#The below code is tells django where the media files are:
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 #The below code is tells django where the static files are:
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
