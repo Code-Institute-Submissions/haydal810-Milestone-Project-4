aec1c66e0c27166891077e855e610d313077088a
David <haydal.tmps@gmail.com>
2019-12-14 19:14:48 +0000

    insert urls & views for search

--- e_store/urls.py
+++ e_store/urls.py
@@ -19,6 +19,7 @@ from accounts.views import index
 from accounts import urls as accounts_urls
 from products import urls as urls_products
 from cart import urls as urls_cart
+from search import urls as urls_search
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -32,5 +33,6 @@ urlpatterns = [
     url(r'^products/', include(urls_products)),
     url(r'^products/', all_products, name='all_products'),
     url(r'^cart/', include(urls_cart)),
+    url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
