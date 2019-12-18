5c52625ad38e63135ff30f90e268ed39b4be6eab
David <haydal.tmps@gmail.com>
2019-12-18 23:32:12 +0000

    apply fix to nav item href to other pages

--- accounts/views.py
+++ accounts/views.py
@@ -63,6 +63,7 @@ def registration(request):
     return render(request, 'registration.html', {
         "registration_form": registration_form})
 
+
 def user_profile(request):
     """The user's profile page"""
     user = User.objects.get(email=request.user.email)
