4c40eeef786d5a5e7570d82b8fbc4d7ff4fe0e59
David <haydal.tmps@gmail.com>
2019-12-22 14:17:06 +0000

    add sendEmail.js and update id's & name's in form

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+function sendMail(contact_form) {
+    emailjs.send("gmail", "milestone_4_project", {
+        "message": contact_form.message.value,
+        "from_name": contact_form.full_name.value,
+        "from_email": contact_form.emailaddress.value,
+        "nature_of_enquiry": contact_form.nature_of_enquiry.value,
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                setTimeout($("#modalMessage").modal(), 500); // the user will know that their message was sent successfully and is given feedback.
+                document.getElementById("contactForm").reset() // the form is reset after submit button is hit
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page...
+}
+
+
